3e699e2f7d53878441b5abe798138f7e77711f98
ameysunu <ameysunu00719@gmail.com>
2020-08-06 22:28:52 +0300

    react files added

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import MainComponent from "./components/MainComponent";
+
+function App() {
+  return (
+    <div className="App">
+      <MainComponent />
+    </div>
+  );
+}
+
+export default App;
